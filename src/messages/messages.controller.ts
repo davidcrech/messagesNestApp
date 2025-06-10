e42172e7bb869d0d9883e9c889556494e9fe385d
davidcrech <davidcrech220@gmail.com>
2025-06-10 14:47:19 -0300

    feat: add service methods to controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -4,16 +4,25 @@ import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesSerivce: MessagesService;
+
+  constructor() {
+    // TODO: use dependency injection instead
+    this.messagesSerivce = new MessagesService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesSerivce.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesSerivce.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messagesSerivce.findOne(id);
   }
 }
